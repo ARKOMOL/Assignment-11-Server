04c5ea42788ab72b001b526f103a21e95d00dd3a
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-07 16:50:09 +0600

    get data

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
+// console.log(process.env)
 const port = process.env.PORT || 5000;
 
 //midlleware 
@@ -11,12 +12,29 @@ app.use(express.json());
 
 /* ==============Cluster Connected========================== */
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wtxlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8dpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8dpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://CarDb:klly2ROL6iHF3uCM@cluster0.s8dpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     console.log('db is connnented');
+//     client.close(); 
+//   });
+
 async function run(){
 try{
+    // console.log('object');
+    await client.connect();
+    const carCollection =client.db("carWarehouse").collection("warehouseProducts");
 
+    app.get('/inventory',async(req,res)=>{
+        const query = {};
+        const cursor = carCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+    })
 }
 finally{
 
